0765ed68f9b0a5784f59d5644f10539631a31fda
lalaalal <100madalala@gmail.com>
2019-05-05 19:55:41 +0900

    .

--- test.py
+++ test.py
@@ -1,35 +1,42 @@
+# by lalaalal
+# date 2019.05.05
+
+import numpy as np
+import matplotlib
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 fig = plt.figure(figsize=(10, 10))
 ax = fig.gca(projection='3d')
 
-x = []
-y = []
+X_REAL = np.arange(-5, 5, 0.1)
+X_IMAG = np.arange(-5, 5, 0.1)
 
-i_min = -5
-while i_min <= 5:
-    r_min = -5
-    while r_min <= 5:
-        tmp = complex(r_min, i_min)
-        x.append(tmp)
-        y.append(tmp ** 2)
-        r_min += 0.1
-    i_min += 0.1
+X_REAL, X_IMAG = np.meshgrid(X_REAL, X_IMAG)
 
-x_r = []
-x_i = []
-y_r = []
-y_i = []
+i = 0
+Y_REAL = []
+Y_IMAG = []
+while i < len(X_REAL):
+    j = 0
+    while j < len(X_REAL[0]):
+        tmp = complex(X_REAL[i][j], X_IMAG[i][j]) ** 2
+        Y_REAL.append(tmp.real)
+        Y_IMAG.append(tmp.imag)
+        j += 1
+    i += 1
 
-for i in x:
-    x_r.append(i.real)
-    x_i.append(i.imag)
+Y_REAL = np.asarray(Y_REAL).reshape((len(X_REAL), len(X_REAL[0])))
+Y_IMAG = np.asarray(Y_IMAG).reshape((len(X_REAL), len(X_REAL[0])))
 
-for j in y:
-    y_r.append(j.real)
-    y_i.append(j.imag)
+minn, maxx = Y_IMAG.min(), Y_IMAG.max()
 
-colormap = y_i
-ax.scatter(x_r, y_r, x_i, s=50, c=colormap)
+norm = matplotlib.colors.Normalize(minn, maxx)
+m = plt.cm.ScalarMappable(norm=norm, cmap='jet')
+fcolors = m.to_rgba(Y_IMAG)
+m.set_array(Y_IMAG)
+fig.colorbar(m)
+
+surf = ax.plot_surface(X_REAL, Y_REAL, X_IMAG, facecolors=fcolors,
+                       vmin=minn, vmax=maxx, shade=False)
 plt.show()
